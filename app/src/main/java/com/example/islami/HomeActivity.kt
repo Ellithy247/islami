bd97e72ed972c3785084172271398407f66b0344
Ellithy <ellithy247@gmail.com>
2022-08-11 18:52:41 +0200

    add splash screen change theme color

--- app/src/main/java/com/example/islami/HomeActivity.kt
+++ app/src/main/java/com/example/islami/HomeActivity.kt
@@ -1,11 +1,15 @@
 package com.example.islami
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
+
+
     }
 }
